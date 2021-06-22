f462fbc13eea0e7da95e2037be0d4a9cc01ca840
Serhii Bura <serhiibura@gmail.com>
2021-06-22 20:53:22 +0300

    guessing_game: Add user input
    Signed-off-by: Serhii Bura <serhiibura@gmail.com>

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing game");
+
+    let mut guess = String::new();
+
+    println!("Try your luck enter a number");
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You are enterred: {}", guess);
 }
