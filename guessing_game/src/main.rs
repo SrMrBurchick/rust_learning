35fb6987e2d0a9c3c443eac3bd991872a51a9d94
Serhii Bura <serhiibura@gmail.com>
2021-06-22 21:03:46 +0300

    guessing_game: Add random nubmer generation
    Signed-off-by: Serhii Bura <serhiibura@gmail.com>

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,14 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guessing game");
 
+    let number = rand::thread_rng().gen_range(1..101);
+
     let mut guess = String::new();
 
-    println!("Try your luck enter a number");
+    println!("Try your luck enter a number in range (1-100)");
 
     io::stdin()
         .read_line(&mut guess)
