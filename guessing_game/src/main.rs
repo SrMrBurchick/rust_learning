087b0c15122bb5eecf6f96f968389d0ebf0e32dc
Serhii Bura <serhiibura@gmail.com>
2021-06-22 21:09:28 +0300

    guessing_game: Update game logick
    Wrap logic to loop
    
    Signed-off-by: Serhii Bura <serhiibura@gmail.com>

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,19 +7,25 @@ fn main() {
 
     let number = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
 
-    println!("Try your luck enter a number in range (1-100)");
+        println!("Try your luck enter a number in range (1-100)");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Not a number");
+        let guess: u32 = guess.trim().parse().expect("Not a number");
 
-    match guess.cmp(&number) {
-        Ordering::Less => println!("You lose, litle cokcsuccer"),
-        Ordering::Greater => println!("You lose, litle cokcsuccer"),
-        Ordering::Equal => println!("Hm... ok, you win"),
+        match guess.cmp(&number) {
+            Ordering::Less => println!("You lose, litle cokcsuccer"),
+            Ordering::Greater => println!("You lose, litle cokcsuccer"),
+            Ordering::Equal => {
+                println!("Hm.. ok, you win");
+                break;
+            }
+        }
+
     }
 }
